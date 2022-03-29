9700b72a091f793f40b385fbf63769d9bdab9a4f
Yudi Zhang <zhan5155@umn.edu>
2022-03-29 17:45:20 -0400

    html contract and order input section added

--- fintech_ibkr/orders_example.py
+++ fintech_ibkr/orders_example.py
@@ -1,3 +1,4 @@
+import time
 
 from ibapi.contract import Contract
 from ibapi.order import Order
@@ -13,17 +14,24 @@ value = "EUR.USD" # This is what your text input looks like on your app
 
 # Create a contract object
 contract = Contract()
-contract.symbol = value.split(".")[0]
-contract.secType  = 'CASH'
-contract.exchange = 'IDEALPRO'  # 'IDEALPRO' is the currency exchange.
-contract.currency = value.split(".")[1]
+# contract.symbol = value.split(".")[0]
+contract.symbol = 'TSLA'
+# look up secType from: https://interactivebrokers.github.io/tws-api/basic_contracts.html
+# contract.secType  = 'CASH'
+contract.secType = 'STK'
+# contract.exchange = 'IDEALPRO'  # 'IDEALPRO' is the currency exchange.
+contract.exchange = 'SMART'
+# contract.currency = value.split(".")[1]
+contract.currency = 'USD'
 
 action = "BUY"
 trade_amt = 10
 order = Order()
 order.action = action
-order.orderType = "MKT"
+# order.orderType = "MKT"
+order.orderType = "LMT"
 order.totalQuantity = trade_amt
+order.lmtPrice = 1010
 
 app = ibkr_app()
 app.connect(hostname, port, client_id)
